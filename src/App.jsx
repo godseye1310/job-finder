66b7d235f21e9af9b9aeec33dd431dedb0cbf1bc
Avinash <avinashkmg1310@gmail.com>
2025-08-26 18:41:01 +0530

    Initialized project using create vite@latest & tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<div className="text-center">
+				<h1 className="text-indigo-600 text-5xl font-bold">
+					Job Finder
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
